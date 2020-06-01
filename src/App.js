37fbda2db69e55f6e95dd4d8410fbd4c0086b1a9
ZachyDev <mosetizachary001@gmail.com>
2020-06-01 23:44:40 +0300

    credit-cards-with-react

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
+      <h1>Credit Cards</h1>
     </div>
   );
 }
---
+++ src/Payment/Payment.js
@@ -0,0 +1,17 @@
+import React, { useState }from 'react'
+import Cards from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
+function Payment() {
+    const [name, setName] = useState('')
+    const [number, setNumber] = useState('')
+    const [expiry, setExpiry] = useState('')
+    const [cvc, setCvc] = useState('')
+    const [focus, setFocus] = useState('')
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Payment
